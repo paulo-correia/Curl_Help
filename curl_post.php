9c62659629b721633ce49b2e72d7e8399d45163c
shivv89 <shivomv89@gmail.com>
2020-10-01 07:19:42 +0530

    Update curl_post.php
    complete curl with post method

--- curl_post.php
+++ curl_post.php
@@ -11,5 +11,13 @@ $str_post = "var_one=".$first_post."&var_two=".$second_post."&var_tree=".$third_
 $ch2=curl_init();
 curl_setopt($ch2,CURLOPT_URL,$url);
 curl_setopt ($ch2, CURLOPT_POST, 1);
+curl_setopt($ch2,CURLOPT_SSL_VERIFYPEER,false); 
+curl_setopt($ch2,CURLOPT_SSL_VERIFYHOST,1);
+curl_setopt($ch2, CURLOPT_SSLVERSION, 6);
 curl_setopt ($ch2, CURLOPT_POSTFIELDS, $str_post);
+$response = curl_exec ($ch);	//print_r($response);		 
+if (curl_errno($ch)) {
+  $error_msg = curl_error($ch);
+}				
+curl_close ($ch);
 
